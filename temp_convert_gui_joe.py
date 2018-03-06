cf0d115a723b1144a574cb89fa0f037b8b1d5418
joetechem <weinzjseiler@gmail.com>
2018-03-06 15:49:15 -0500

    instructor

---
+++ temp_convert_gui_joe.py
@@ -0,0 +1,13 @@
+from graphics import *
+
+def main():
+    win = GraphWin("Celsius Converter", 400, 500)
+    win.setCoords(0.0, 0.0, 3.0, 4.0)
+
+    # Display the output and exit
+    win.getMouse()
+    win.close()
+
+    
+
+main()
